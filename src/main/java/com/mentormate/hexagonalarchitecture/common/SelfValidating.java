eb75807a14dfca4ba5390335d8bbff6b1adddf8a
Stefan Angelov <stefan.angelov@mentormate.com>
2020-05-07 15:59:31 +0300

    add common components

---
+++ src/main/java/com/mentormate/hexagonalarchitecture/common/SelfValidating.java
@@ -0,0 +1,29 @@
+package com.mentormate.hexagonalarchitecture.common;
+
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+public abstract class SelfValidating<T> {
+
+    private Validator validator;
+
+    public SelfValidating() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    /**
+     * Evaluates all Bean Validations on the attributes of this
+     * instance.
+     */
+    protected void validateSelf() {
+        Set<ConstraintViolation<T>> violations = validator.validate((T) this);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
+    }
+}
